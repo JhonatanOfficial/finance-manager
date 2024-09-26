dad7bf5e99f9194ceb7ea2ee538634def0d2eab5
Jhonatan-S <jhonatan@jhonatandev.com.br>
2024-09-26 18:50:47 -0300

    fix error vercel firebase-admin

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,9 +3,9 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-        clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
+        clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL as string,
+        privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') as string,
     }),
    
   });
